76ba768e79ff67f38e904bcfb5bc7c5fe7f3f86f
ClemCa <perso.catorc@gmail.com>
2022-04-03 01:37:34 +0200

    cloud (hopefully) last fix

--- Assets/CloudMoveScript.cs
+++ Assets/CloudMoveScript.cs
@@ -1,3 +1,4 @@
+using ClemCAddons;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -21,14 +22,38 @@ public class CloudMoveScript : MonoBehaviour
         transform.position += Vector3.right * speed * Time.deltaTime;
 
         // Ensure that the cloud can always be seen by the main camera
-        Vector3 worldCameraCorner = Camera.main.ViewportToWorldPoint(Vector3.zero);
-        Bounds bounds = gameObject.GetComponent<MeshFilter>().sharedMesh.bounds;
+        var ray = Camera.main.ViewportPointToRay(new Vector3(0,0.5f));
 
-        if (worldCameraCorner.x > bounds.max.x) {
-            float diff = worldCameraCorner.x - bounds.max.x;
+        Bounds bounds = gameObject.GetComponent<Collider>().bounds;
 
+        LineLineIntersection(out Vector3 worldCameraCorner, ray.origin, ray.direction, Camera.main.transform.position.SetZ(0), Vector3.left);
+
+        if (worldCameraCorner.x > transform.position.x + bounds.extents.x)
+        {
+            float diff = worldCameraCorner.x - (transform.position.x + bounds.extents.x);
             // The bounds center may not be the same as the transform position
             transform.position += Vector3.right * diff;
+        }
+    }
+    public static bool LineLineIntersection(out Vector3 intersection, Vector3 linePoint1, Vector3 lineVec1, Vector3 linePoint2, Vector3 lineVec2)
+    {
+        Vector3 lineVec3 = linePoint2 - linePoint1;
+        Vector3 crossVec1and2 = Vector3.Cross(lineVec1, lineVec2);
+        Vector3 crossVec3and2 = Vector3.Cross(lineVec3, lineVec2);
+
+        float planarFactor = Vector3.Dot(lineVec3, crossVec1and2);
+
+        //is coplanar, and not parallel
+        if (Mathf.Abs(planarFactor) < 0.0001f && crossVec1and2.sqrMagnitude > 0.0001f)
+        {
+            float s = Vector3.Dot(crossVec3and2, crossVec1and2) / crossVec1and2.sqrMagnitude;
+            intersection = linePoint1 + (lineVec1 * s);
+            return true;
+        }
+        else
+        {
+            intersection = Vector3.zero;
+            return false;
         }
     }
 }
