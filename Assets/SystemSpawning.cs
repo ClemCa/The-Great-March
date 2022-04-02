c942285186e81f956702ce71be3fe07f551cac5a
ClemCa <perso.catorc@gmail.com>
2022-04-02 16:26:02 +0200

    generation system complete

--- Assets/SystemSpawning.cs
+++ Assets/SystemSpawning.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ClemCAddons;
 
 public class SystemSpawning : MonoBehaviour
 {
     [SerializeField] private GameObject _systemPrefab;
     [SerializeField] private float _every;
+    [SerializeField] private int _howManyBeforehand = 2;
+    [SerializeField] private int _maxSystems = 10;
+    [SerializeField] private List<GameObject> _spawned = new List<GameObject>();
+
     private float _last = 0;
 
     void Update()
     {
-        if(transform.position.x > _last + _every)
+        if(transform.position.x + _every * _howManyBeforehand > _last)
         {
-            _last = transform.position.x;
+            _last += _every;
+            _spawned.Add(Instantiate(_systemPrefab, new Vector3(_last, 0), Quaternion.identity));
             Debug.Log("instantiate prefab now");
+        }
+        if(_spawned.Count > _maxSystems)
+        {
+            Destroy(_spawned[0]);
+            _spawned.RemoveAt(0);
         }
     }
 }
