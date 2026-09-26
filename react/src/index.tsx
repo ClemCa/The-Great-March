c865d71863f8cebc43c812b893eaf8dd5d42cba7
ClemCa <perso.catorc@gmail.com>
2026-09-26 04:33:10 -0400

    Installed ReactUnity

---
+++ react/src/index.tsx
@@ -0,0 +1,12 @@
+import { render } from '@reactunity/renderer';
+import './index.scss';
+
+function App() {
+  return (
+    <scroll>
+      <text>{'Go to <color=red>src/index.tsx</color> to edit this file'}</text>
+    </scroll>
+  );
+}
+
+render(<App />);
